919ae21f0b996fa436816f07bc4f68a65c7aa300
Denis <brezgin.denis@list.ru>
2024-09-26 18:23:33 +0700

    all

--- games/gcd.js
+++ games/gcd.js
@@ -1,7 +1,8 @@
 import game from "../src/index.js";
 import randomNum from "../src/getRandomInt.js";
 const gcd = () => {
-  const nameGame = "Find the greatest common divisor of given numbers.";
+  const nameGame =
+    'Answer "yes" if given number is prime. Otherwise answer "no".';
   const taskGcd = () => {
     const num1 = randomNum(30);
     const num2 = randomNum(50);
