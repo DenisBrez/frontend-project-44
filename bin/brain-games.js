80d46432fee98c2e16aeab79b67fc14bdebaec22
Denis <brezgin.denis@list.ru>
2024-09-30 21:06:58 +0700

    all

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-// eslint-disable-next-line max-len
-// eslint-disable-next-line import/extensions, import/no-named-as-default, import/no-named-as-default-member
-import userName from '../src/games/cli.js';
+
+import userName from '../src/games/cli.js'; 
 
 // eslint-disable-next-line no-console
 console.log(`Hello, ${userName}!`);
